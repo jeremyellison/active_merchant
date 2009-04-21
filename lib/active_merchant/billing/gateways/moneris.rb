87c8d0ae0341880d101dd7617ce17add08ce4f7d
Jeremy Ellison <jeremy@3cubetechnologies.com>
2009-04-21 17:00:18 -0400

    moneris USA support

--- lib/active_merchant/billing/gateways/moneris.rb
+++ lib/active_merchant/billing/gateways/moneris.rb
@@ -10,10 +10,12 @@ module ActiveMerchant #:nodoc:
     # Response Values", available at Moneris' {eSelect Plus Documentation 
     # Centre}[https://www3.moneris.com/connect/en/documents/index.html].
     class MonerisGateway < Gateway
-      TEST_URL = 'https://esqa.moneris.com/gateway2/servlet/MpgRequest'
-      LIVE_URL = 'https://www3.moneris.com/gateway2/servlet/MpgRequest'
+      #TEST_URL = 'https://esqa.moneris.com/gateway2/servlet/MpgRequest'
+      #LIVE_URL = 'https://www3.moneris.com/gateway2/servlet/MpgRequest'
+      TEST_URL = 'https://esplusqa.moneris.com/gateway_us/servlet/MpgRequest'
+      LIVE_URL = 'https://esplus.moneris.com/gateway_us/servlet/MpgRequest'
       
-      self.supported_countries = ['CA']
+      self.supported_countries = ['US']
       self.supported_cardtypes = [:visa, :master, :american_express]
       self.homepage_url = 'http://www.moneris.com/'
       self.display_name = 'Moneris'
@@ -32,7 +34,7 @@ module ActiveMerchant #:nodoc:
       # 
       # Pass in +order_id+ and optionally a +customer+ parameter.
       def authorize(money, creditcard, options = {})
-        debit_commit 'preauth', money, creditcard, options        
+        debit_commit 'us_preauth', money, creditcard, options        
       end
       
       # This action verifies funding on a customer's card, and readies them for 
@@ -40,7 +42,7 @@ module ActiveMerchant #:nodoc:
       # 
       # Pass in <tt>order_id</tt> and optionally a <tt>customer</tt> parameter
       def purchase(money, creditcard, options = {})
-        debit_commit 'purchase', money, creditcard, options
+        debit_commit 'us_purchase', money, creditcard, options
       end
      
       # This method retrieves locked funds from a customer's account (from a 
@@ -51,7 +53,7 @@ module ActiveMerchant #:nodoc:
       # gateways the two numbers are concatenated together with a ; separator as
       # the authorization number returned by authorization
       def capture(money, authorization, options = {})
-        commit 'completion', crediting_params(authorization, :comp_amount => amount(money))
+        commit 'us_completion', crediting_params(authorization, :comp_amount => amount(money))
       end
 
       # Voiding requires the original transaction ID and order ID of some open 
@@ -62,7 +64,7 @@ module ActiveMerchant #:nodoc:
       # (';'). This is to keep the Moneris interface consistent with other 
       # gateways. (See +capture+ for details.)
       def void(authorization, options = {})
-        commit 'purchasecorrection', crediting_params(authorization)
+        commit 'us_purchasecorrection', crediting_params(authorization)
       end
       
       # Performs a refund. This method requires that the original transaction 
@@ -71,7 +73,7 @@ module ActiveMerchant #:nodoc:
       # Moneris interface consistent with other gateways. (See +capture+ for 
       # details.)
       def credit(money, authorization, options = {})
-        commit 'refund', crediting_params(authorization, :amount => amount(money))
+        commit 'us_refund', crediting_params(authorization, :amount => amount(money))
       end
    
     private # :nodoc: all
@@ -117,7 +119,15 @@ module ActiveMerchant #:nodoc:
       end
   
       def commit(action, parameters = {})
-        response = parse(ssl_post(test? ? TEST_URL : LIVE_URL, post_data(action, parameters)))
+        raw_response = ssl_post(test? ? TEST_URL : LIVE_URL, post_data(action, parameters))
+        # puts "========================"
+        # puts post_data(action, parameters)
+        # puts "TO: #{test? ? TEST_URL : LIVE_URL}"
+        # puts "yields:"
+        # puts raw_response
+        # puts "========================"
+        # 
+        response = parse(raw_response)
 
         Response.new(successful?(response), message_from(response[:message]), response,
           :test          => test?,
@@ -134,8 +144,8 @@ module ActiveMerchant #:nodoc:
       
       # Tests for a successful response from Moneris' servers
       def successful?(response)
-        response[:response_code] && 
         response[:complete] && 
+        response[:response_code] && 
         (0..49).include?(response[:response_code].to_i)
       end
                                                
@@ -185,16 +195,16 @@ module ActiveMerchant #:nodoc:
       
       def actions
         {
-          "purchase"           => [:order_id, :cust_id, :amount, :pan, :expdate, :crypt_type],
-          "preauth"            => [:order_id, :cust_id, :amount, :pan, :expdate, :crypt_type],
-          "command"            => [:order_id],
-          "refund"             => [:order_id, :amount, :txn_number, :crypt_type],
-          "indrefund"          => [:order_id, :cust_id, :amount, :pan, :expdate, :crypt_type],
-          "completion"         => [:order_id, :comp_amount, :txn_number, :crypt_type],
-          "purchasecorrection" => [:order_id, :txn_number, :crypt_type],
-          "cavvpurcha"         => [:order_id, :cust_id, :amount, :pan, :expdate, :cav],
-          "cavvpreaut"         => [:order_id, :cust_id, :amount, :pan, :expdate, :cavv],
-          "transact"           => [:order_id, :cust_id, :amount, :pan, :expdate, :crypt_type],
+          "us_purchase"           => [:order_id, :cust_id, :amount, :pan, :expdate, :crypt_type],
+          "us_preauth"            => [:order_id, :cust_id, :amount, :pan, :expdate, :crypt_type],
+          "us_command"            => [:order_id],
+          "us_refund"             => [:order_id, :amount, :txn_number, :crypt_type],
+          "us_indrefund"          => [:order_id, :cust_id, :amount, :pan, :expdate, :crypt_type],
+          "us_completion"         => [:order_id, :comp_amount, :txn_number, :crypt_type],
+          "us_purchasecorrection" => [:order_id, :txn_number, :crypt_type],
+          "us_cavvpurcha"         => [:order_id, :cust_id, :amount, :pan, :expdate, :cav],
+          "us_cavvpreaut"         => [:order_id, :cust_id, :amount, :pan, :expdate, :cavv],
+          "us_transact"           => [:order_id, :cust_id, :amount, :pan, :expdate, :crypt_type],
           "Batchcloseall"      => [],
           "opentotals"         => [:ecr_number],
           "batchclose"         => [:ecr_number]
